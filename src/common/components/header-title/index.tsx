6e9ef890697dbeda7ef1868f05e320cc581781b5
GaussYuan191 <1915059302@qq.com>
2022-06-17 10:48:09 +0800

    新增：头部标题组件

---
+++ src/common/components/header-title/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+interface TittleProps {
+  title: string
+}
+function HeaderTittle (props: TittleProps) {
+  return <div className="header-title">
+    {props.title}
+  </div>
+}
+
+export default HeaderTittle
