9d1613fd92ee629e4e06851e48e7783f9f16389c
GaussYuan191 <1915059302@qq.com>
2022-06-17 10:40:08 +0800

    新增：主页内容

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,38 @@
-const Home = () => {
-  return <div>主页</div>;
+import { FC } from "react";
+import { Card, Col, Row } from 'antd';
+import PageLayout from '../../common/components/page-layout'
+const Home: FC = () => {
+  const getRadom = (unit: number) => {
+    const limit = (Math.pow(10, unit) * Math.random()).toString();
+    return parseInt(limit);
+  }
+  return <PageLayout title='首页'>
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={6}>
+          <Card title="总访问量" extra={<span className="day">日</span>}>
+            <p className="viewdata">{getRadom(4)}</p>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="总评论数" extra={<span className="day">日</span>}>
+            <p className="viewdata">{getRadom(2)}</p>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="总留言数" extra={<span className="day">日</span>}>
+            <p className="viewdata">{getRadom(2)}</p>
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card title="关于页访问量" extra={<span className="day">日</span>}>
+            <p className="viewdata">{getRadom(4)}</p>
+          </Card>
+        </Col>
+      </Row>
+      
+    </div>
+  </PageLayout>
 };
 
 export default Home;
