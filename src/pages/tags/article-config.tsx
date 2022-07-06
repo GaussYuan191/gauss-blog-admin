1d851a7ebd30054995e0b4f5e9e219003fdf21d0
GaussYuan191 <1915059302@qq.com>
2022-07-06 16:40:07 +0800

    新增：标签管理页面

---
+++ src/pages/tags/article-config.tsx
@@ -0,0 +1,32 @@
+import { ColumnProps } from "antd/es/table";
+
+interface ColumnPropsEditTable<T> extends ColumnProps<T> {
+  editable?: boolean;
+}
+
+export const columns:Array<ColumnPropsEditTable<any>> = [
+  {
+    title: '#',
+    width: 50,
+    key: 'tindex',
+    render: (text, record, dataIndex) => <span>{dataIndex+1}</span>
+  },
+  {
+    title: '标题',
+    dataIndex: 'name',
+    width: 400,
+    key: 'name',
+    editable: true
+  },
+  {
+    title: '描述',
+    key: 'descript',
+    dataIndex: 'descript',
+    editable: true
+  },
+  {
+    title: '编辑',
+    dataIndex: 'operation',
+    width: 150
+  }
+]
