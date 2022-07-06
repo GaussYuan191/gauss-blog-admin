b4973e4b6c60801c791a68710b3b8ec9cd513370
GaussYuan191 <1915059302@qq.com>
2022-07-06 17:17:05 +0800

    修改：完善文章列表

--- src/pages/article-list/article-config.tsx
+++ src/pages/article-list/article-config.tsx
@@ -1,62 +1,77 @@
-import { Tag } from 'antd'
-import type { ColumnsType } from 'antd/lib/table';
-import dayjs from 'dayjs'
-export interface recordType{
-  name: string
+import React from "react";
+import { Tag } from "antd";
+import dayjs from "dayjs";
+export interface recordType {
+  name: string;
 }
-interface dataList {
-  
-}
-const colorArr = ['magenta', 'geekblue', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'purple']
-export const columns:ColumnsType = [
+
+const colorArr = [
+  "magenta",
+  "geekblue",
+  "red",
+  "volcano",
+  "orange",
+  "gold",
+  "lime",
+  "green",
+  "cyan",
+  "purple",
+];
+export const columns = [
   {
-    title: '#',
+    title: "#",
     width: 50,
-    key: 'tindex',
-    render: (text: string, record: any, dataIndex: number) => <span>{dataIndex+1}</span>
+    key: "tindex",
+    render: (text: string, record: any, dataIndex: number) => (
+      <span>{dataIndex + 1}</span>
+    ),
   },
   {
-    title: '标题',
+    title: "标题",
     width: 280,
-    dataIndex: 'title',
-    key: 'title',
+    dataIndex: "title",
+    key: "title",
     ellipsis: true,
   },
   {
-    title: '标签',
-    key: 'tag',
+    title: "标签",
+    key: "tag",
     width: 250,
     render: (record: any) => {
       return (
         <span>
-          {record.tag.map((item:any, index: number) => {
+          {record.tag.map((item: any, index: number) => {
             return (
-              <Tag color={colorArr[index] || 'red'} key={item._id}>
+              <Tag color={colorArr[index] || "red"} key={item._id}>
                 {item.name}
               </Tag>
-            )
+            );
           })}
         </span>
-      )
-    }
+      );
+    },
   },
   {
-    title: '更新日期',
+    title: "更新日期",
     width: 250,
-    dataIndex: 'update_at',
-    key: 'update_at',
-    render: (text: any) => <div>{dayjs(text).format('YYYY-MM-DD HH:mm:ss')}</div> 
+    dataIndex: "update_at",
+    key: "update_at",
+    render: (text: any) => (
+      <div>{dayjs(text).format("YYYY-MM-DD HH:mm:ss")}</div>
+    ),
   },
   {
-    title: '信息',
-    dataIndex: 'meta',
-    key: 'meta',
-    render: (meta: any) => <div>{`评论：${meta.comments}、浏览：${meta.views}、喜欢：${meta.likes}`}</div>
+    title: "信息",
+    dataIndex: "meta",
+    key: "meta",
+    render: (meta: any) => (
+      <div>{`评论：${meta.comments}、浏览：${meta.views}、喜欢：${meta.likes}`}</div>
+    ),
   },
   {
-    title: '编辑',
-    key: 'operation',
-    dataIndex: 'operation',
-    width: 150
-  }
-]
+    title: "编辑",
+    key: "operation",
+    dataIndex: "operation",
+    width: 150,
+  },
+];
--- src/pages/article-list/index.tsx
+++ src/pages/article-list/index.tsx
@@ -1,18 +1,24 @@
 import { RouteComponentProps } from "react-router";
 import { useState } from "react";
 import { useRequest, useMount } from "ahooks";
-import { Table } from "antd";
+import { Table, Divider, message } from "antd";
+import { TablePaginationConfig } from "antd/es/table";
 import PageLayout from "../../common/components/page-layout";
 
 import { columns } from "./article-config";
 import { getArts, delArt } from "../../utils";
+import "./index.scss";
+
+type EditableTableProps = Parameters<typeof Table>[0];
+type ColumnTypes = Exclude<EditableTableProps["columns"], undefined>;
 
 const ArticleList = (props: RouteComponentProps) => {
   const [list, setList] = useState([]);
   const [page, setPage] = useState({ current: 1, total: 0 });
   const [loading, setLoading] = useState(true);
+  const [load, setLoad] = useState(0);
 
-  const pageList = (): Promise<object> => {
+  const pageList = (): Promise<any> => {
     return new Promise(async (resolve, reject) => {
       let { data } = await getArts();
       console.log("data", data);
@@ -22,21 +28,84 @@ const ArticleList = (props: RouteComponentProps) => {
   };
   const { run } = useRequest(pageList, {
     manual: true,
-    onSuccess: (result: object) => {
-      console.log(result);
+    onSuccess: (data: any) => {
+      const { list, pagination } = data;
+      setList(list || []);
+      setPage({ current: pagination.current_page, total: pagination.total });
+      setLoading(false);
     },
     onError: (error) => {
       console.log(error.message);
+      setLoading(false);
     },
   });
   useMount(() => {
-    run()
+    run();
   });
+  // 编辑
+  async function edit(id: string) {
+    props.history.push(`/article-add?id=${id}`);
+  }
+  // 删除
+  async function del(id: string) {
+    const { data } = await delArt(id);
+    if (data.code) {
+      message.success(data.message);
+      setPage({ current: 1, total: 0 });
+      setLoad(load + 1);
+    }
+  }
+  const tableColumns = columns.map((col) => {
+    if (col.dataIndex === "operation") {
+      return {
+        ...col,
+        onCell: (record: any) => ({ col, record, edit, del }),
+      };
+    }
+    return col;
+  });
+  const components = { body: { cell: TCell } };
+  function tableChange(pagination: TablePaginationConfig) {
+    setPage({ current: pagination.current || 1, total: pagination.total || 0 });
+    setLoad(load + 1);
+  }
+
   return (
     <PageLayout title="文章列表">
-      {/* <Table columns={columns} pagination={{ position: [top, bottom] }} dataSource={data} /> */}
+      <Table
+        onChange={tableChange}
+        components={components}
+        columns={tableColumns as ColumnTypes}
+        loading={loading}
+        dataSource={list}
+        bordered
+        size="middle"
+        pagination={page}
+        rowKey="_id"
+      />
     </PageLayout>
   );
 };
+function TCell(props: any) {
+  const { children, col } = props;
+  let res: any =
+    col && col.dataIndex === "operation" ? <Operate {...props} /> : children;
+  return <td>{res}</td>;
+}
+export function Operate(props: any) {
+  const { edit, del, record } = props;
+  const view = () => {
+    console.log("view");
+  };
+  return (
+    <div className="t_btn">
+      <span onClick={view}>查看</span>
+      <Divider type="vertical" />
+      <span onClick={() => edit(record._id)}>修改</span>
+      <Divider type="vertical" />
+      <span onClick={() => del(record._id)}>删除</span>
+    </div>
+  );
+}
 
 export default ArticleList;
