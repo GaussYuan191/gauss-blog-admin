5abc4392e5a543bba687813157f128cec3fd4741
dengsuyuan <dengsuyuan@ysstech.com>
2022-08-31 17:14:43 +0800

    修改：添加部署地址，更新打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 配置前端服务地址和端口
+  server: {
+    host: '0.0.0.0',
+    port: 9090,
+    // 是否开启 https
+    https: false,
+  },
+  base:'./',  
 });
