5c06e3795b54b0f40327d88aa034d7a15cad35ae
dengsuyuan <dengsuyuan@ysstech.com>
2022-08-31 20:22:27 +0800

    修改：添加提示

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './common/scss/reset.scss';
+// 入口
 ReactDOM.render(
   <React.StrictMode>
     <App />
