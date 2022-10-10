da061fbe58a2f9d53a3eb5d68d9d5e9e915a25b4
dengsuyuan <dengsuyuan@ysstech.com>
2022-10-10 14:05:50 +0800

    feat: 新增留言新增页面

---
+++ src/pages/message-add/index.tsx
@@ -0,0 +1,9 @@
+import PageLayout from '../../common/components/page-layout';
+
+export default function AddMessage() {
+  return (
+    <PageLayout title="新增留言">
+      <h1>新增留言(TODO)</h1>
+    </PageLayout>
+  );
+}
