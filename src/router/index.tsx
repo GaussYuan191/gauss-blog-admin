d2b4a5f3e906b50643b83a85e4a34920eb5db385
dengsuyuan <dengsuyuan@ysstech.com>
2022-10-10 14:06:36 +0800

    feat: 添加路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -10,10 +10,24 @@ const Home = loadable(() => import('../pages/home'));
 const ArticleList = loadable(() => import('../pages/article-list'));
 // 文章新增
 const AddArticle = loadable(() => import('../pages/article-add'));
+// 文章分类
+const ArticleClassify = loadable(() => import('../pages/article-classify'));
+// 草稿箱
+const ArticleDraft = loadable(() => import('../pages/article-draft'));
+// 评论管理
+const ArticleComments = loadable(() => import('../pages/article-comments'));
 // 标签列表
 const Tags = loadable(() => import('../pages/tags'));
 // 标签新增
 const TagAdd = loadable(() => import('../pages/tag-add'));
+// 新增留言
+const AddMessage = loadable(() => import('../pages/message-add'));
+// 留言板
+const MessageList = loadable(() => import('../pages/messages-list'));
+// 项目新增
+const ProjectAdd = loadable(() => import('../pages/project-add'));
+// 项目列表
+const Projects = loadable(() => import('../pages/projects'));
 /** 导出路由配置 */
 export const routes = [
   {
@@ -32,42 +46,34 @@ export const routes = [
         path: '/article-add',
         component: AddArticle,
       },
-      // {
-      //   path: "/article-classify",
-      //   component: ArticleClassify,
-      // },
-      // {
-      //   path: "/discuss",
-      //   component: ArticleComments,
-      // },
-      // {
-      //   path: "/article-draft",
-      //   component: ArticleDraft,
-      // },
-      // {
-      //   path: "/message-add",
-      //   component: AddMessage,
-      // },
-      // {
-      //   path: "/message",
-      //   component: MessageList,
-      // },
-      // {
-      //   path: "/music-add",
-      //   component: MusicAdd,
-      // },
-      // {
-      //   path: "/music",
-      //   component: MusicList,
-      // },
-      // {
-      //   path: "/project-add",
-      //   component: ProjectAdd,
-      // },
-      // {
-      //   path: "/project",
-      //   component: Projects,
-      // },
+      {
+        path: '/article-classify',
+        component: ArticleClassify,
+      },
+      {
+        path: '/discuss',
+        component: ArticleComments,
+      },
+      {
+        path: '/article-draft',
+        component: ArticleDraft,
+      },
+      {
+        path: '/message-add',
+        component: AddMessage,
+      },
+      {
+        path: '/message',
+        component: MessageList,
+      },
+      {
+        path: '/project-add',
+        component: ProjectAdd,
+      },
+      {
+        path: '/project',
+        component: Projects,
+      },
       {
         path: '/tags',
         component: Tags,
